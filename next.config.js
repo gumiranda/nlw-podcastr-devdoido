082f03f08dfddd2190c1cdf0d55f44acc1085de3
gumiranda <imartytk@hotmail.com>
2021-04-16 18:54:57 -0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
-  future: {
-    webpack5: true,
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
   },
-}
+  future: {
+    webpack5: true
+  }
+});
