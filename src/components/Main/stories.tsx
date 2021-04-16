a7a042d7820dbb90f5b6d00980aeacaa866c91cd
gumiranda <imartytk@hotmail.com>
2021-04-16 18:50:27 -0300

    story refactor

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,7 +1,16 @@
 import Main from './Main';
+import { Story, Meta } from '@storybook/react/types-6-0';
 export default {
   title: 'Main',
+  /* forma alternativa de passar args
+  args: { title: 'lalala', description: 'nnnn' },
+   */
   component: Main
-};
+} as Meta;
 
-export const Basic = () => <Main />;
+export const Basic: Story = (args) => <Main {...args} />;
+
+Basic.args = {
+  title: 'React Avançado',
+  description: 'Typescript, ReactJS, NextJS e Styled Components'
+};
