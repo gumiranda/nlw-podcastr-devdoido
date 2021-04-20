ec21d51730874545b65bcc1511b198452133d86e
Gustavo Miranda <imartytk@hotmail.com>
2021-04-20 18:23:46 -0300

    formatting

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,15 +4,11 @@ import Main from './Main';
 describe('<Main/>', () => {
   it('should render the heading', () => {
     render(<Main />);
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /react avançado/i })).toBeInTheDocument();
   });
   it('should render correctly', () => {
     const { container } = render(<Main />);
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /react avançado/i })).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
   });
   it('should render colors correctly', () => {
