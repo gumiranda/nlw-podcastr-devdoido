a2ab6dfc3e7a0db70db0fa07d2a51fa27e39ba17
Gustavo Miranda <gustavo.aguiar@redspark.io>
2021-04-24 10:39:08 -0300

    testing passing

--- src/components/LatestEpisodesPodcast/test.tsx
+++ src/components/LatestEpisodesPodcast/test.tsx
@@ -4,9 +4,11 @@ import LatestEpisodesPodcast from './LatestEpisodesPodcast';
 
 describe('<LatestEpisodesPodcast />', () => {
   it('should render the heading', () => {
-    const { container } = render(<LatestEpisodesPodcast />);
+    const { container } = render(
+      <LatestEpisodesPodcast title={'Últimos lançamentos'} latestEpisodes={[]} />
+    );
     expect(
-      screen.getByRole('heading', { name: /LatestEpisodesPodcast/i })
+      screen.getByRole('heading', { name: /Últimos lançamentos/i })
     ).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
   });
